a1ed6f57801ac28daf4159f2a7e9e6e15229f14a
Meir Snyder <msny36@gmail.com>
2015-03-23 13:45:55 -0700

    Added Eric's stuff

--- mascotConstructor.js
+++ mascotConstructor.js
@@ -5,3 +5,9 @@ var Mascot = function(name, food, endurance){
 	
 	
 }
+this.run(endurance){
+    var distance = Math.floor(Math.random()*endurance);
+    console.log(this.name+" the "+this.food+" ran "+distance+" meters.")
+}
+//returns a variable called distance which is equal to a random number between 1 and the mascot's endurance
+//logs to the console "NAME the FOODITEM ran DISTANCE meters!
