5cdd1fdab8e227ab80de20d25a8eceba1c14013b
Meir Snyder <msny36@gmail.com>
2015-03-23 14:20:46 -0700

    Added TotalDistance

--- mascotConstructor.js
+++ mascotConstructor.js
@@ -9,6 +9,8 @@ var Mascot = function(name, food, endurance){
 this.run(endurance){
     var distance = Math.floor(Math.random()*endurance);
     console.log(this.name+" the "+this.food+" ran "+distance+" meters.")
+    return totalDistance += distance;
+    console.log(this.totalDistance);
 }
 //returns a variable called distance which is equal to a random number between 1 and the mascot's endurance
 //logs to the console "NAME the FOODITEM ran DISTANCE meters!
